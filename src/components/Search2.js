6007387eeb98a44f26bb2c9e6338d357a1c5e056
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-30 20:02:36 +0530

    2nd Approach React Ref Done

--- src/components/Search2.js
+++ src/components/Search2.js
@@ -1,13 +1,24 @@
-//Approach 2 : React Rest
-import React from 'react'
+//Approach 2 : React Ref
+ import React from 'react'
 
 export default class Search2 extends React.Component{
+  //Create a variable for react ref
+  //where ever this is included it creates a reference for that element
+    inputRef = React.createRef()
 
+    handleClick = () =>{
+        //inputRef.current refer comments in 19
+        const value = this.inputRef.current.value
+            alert(`The input value is ${value}`)
+    }
     render(){
         return (
             <div>
-            <h1>Search Box 2 :React Rest </h1>
-            <input type="text" placeholder="SearchBox2"/>
+            <h1>Search Box 2 :React Ref </h1>
+            <br />
+            {/* when an  ref is passed in the render() that can be accessible via inputRef.current */}
+            <input ref = {this.inputRef} type="text"  name = "username" placeholder="SearchBox2"/>
+            <button onClick = {this.handleClick}>Click Me</button>
             </div>
             
         ) 
