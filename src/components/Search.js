f919f7f87db8a98bdb0a84fb31921c050fb8dcfe
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-30 14:42:21 +0530

    state with and without constructor

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,22 @@
 import React from 'react'
 
-export default function Search(){
-    return "Hello World"
+export default class Search extends React.Component{
+  //With constructor
+    constructor(props){
+        super(props)
+
+        this.state = {
+         userName :'',   
+        }     
+    }
+
+   //With out constructor and 
+//    state = {
+//        userName : "",
+//    } 
+   
+    render(){
+        
+        return (<input value = {this.state.userName} type = "text" placeholder="Enter UserName"/>)
+    }
 }
