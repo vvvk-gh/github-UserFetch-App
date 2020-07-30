c1a373b4521587628e2e342fb37c4287ef63e740
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-30 11:54:05 +0530

    added search  component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
+    <Search />
   );
 }
 
