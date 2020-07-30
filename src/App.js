12a8bbdb561af3a0648bede840f9b3ad1b9a7329
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-31 00:49:28 +0530

    fetching theUser  data from github

--- src/App.js
+++ src/App.js
@@ -4,10 +4,34 @@ import Search from './components/Search'
 
 
 class App extends React.Component {
-  
-  fetchUserData(username){
-    //fetch user data  
-    alert(username);
+//state without constructor as constructor is not mandatory
+  state =  {
+    user : null
+  }
+
+  async fetchUserData(username){
+    //fetch git hub user data
+
+      try{
+        //fetch is method provided by browsers
+          const res = await fetch(`https://api.github.com/users/${username}`) 
+          if(res.ok){
+              const data = await res.json();
+              //console.log(data);
+              return (
+                  this.setState({
+                  user : data
+                })
+              )
+
+          }
+          else{
+              console.log(`User doesn't exist`)
+          }
+        }catch (err){
+            console.log(`${err}`)
+        }
+    
   }
   
   render(){
