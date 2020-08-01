0b553d5a123bf598895d710087442892f18c9b65
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-01 21:07:48 +0530

    refactored the code and fetch repos method

--- src/App.js
+++ src/App.js
@@ -10,27 +10,38 @@ class App extends React.Component {
     loading : false
   }
 
-  async fetchUserData(username){
-    //this.setState({//can update options} , optional callback ()=>{})
-    //we are going to use that callback for loading
+ fetchUserData = async(username) =>{
+  const res = await fetch(`https://api.github.com/users/${username}`) 
+      if(res.ok){
+          const data = await res.json();      
+          return {data} 
+      }
+  const error = (await res.json()).message;
+    return {error};
+ }
+
+ fetchRepos = async (username) =>{
+  const res = await fetch (`https://api.github.com/users/${username}/repos?page=1`)
+      if(res.ok){
+        const data = await res.json();      
+        return {data} 
+      }
+
+    const error = (await res.json()).message;
+    return {error};
+
+ }
+
+
+  async fetchData(username){
     this.setState({loading : true} , async ()=>{
-     
       try{
-        const res = await fetch(`https://api.github.com/users/${username}`) 
-        if(res.ok){
-            const data = await res.json();
-            
-            return (
-                this.setState({
-                user : data,
-                error : null,
-                loading: false,
-              })
-            )
+        const {data , error} = await this.fetchUserData(username); 
+
+        if(data){
+          this.state({  user:data,loading: false})
         }
 
-        const error = (await res.json()).message;
-        
         this.setState({
           error,
           loading : false,
@@ -46,12 +57,14 @@ class App extends React.Component {
 
       })
   }
+
+
   
   render(){
   const {error , loading , user} = this.state;
     return (
       <div>
-      <Search fetchData = {(username) => this.fetchUserData(username)} />
+      <Search fetchData = {(username) => this.fetchData(username)} />
       <div className="text-center pt-5">
       {error && <p className = "text-danger">{error}</p>}
       {loading && <p>Loading...</p>}
